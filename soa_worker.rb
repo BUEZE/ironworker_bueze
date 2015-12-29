5c56d18db4a9c97ab5d3cc091336bb47cfd6f1b4
Hiraku <dev@hiraku.tw>
2015-12-29 17:03:00 +0800

    post message

--- soa_worker.rb
+++ soa_worker.rb
@@ -9,7 +9,9 @@ puts "Setting up AWS connection"
 config = JSON.parse(File.read('config/config.json'))
 ENV.update config
 sqs = Aws::SQS::Client.new
+msg = {url: 'http://bueze.herokuapp.com/api/v1/bookranking/', body: '{"source":"bueze"}'}
 q_url = sqs.get_queue_url(queue_name: 'SOA_queue').queue_url
+msg_sent = sqs.send_message(queue_url: q_url, message_body: msg.to_json)
 
 puts "Polling SQS for messages"
 poller = Aws::SQS::QueuePoller.new(q_url)
@@ -17,7 +19,10 @@ begin
   poller.poll(wait_time_seconds:nil, idle_timeout:5) do |msg|
     puts "MESSAGE: #{JSON.parse(msg.body)}"
     req = JSON.parse msg.body
-    results = HTTParty.get req['url']
+    # results = HTTParty.get req['url']
+    results = HTTParty.post(req['url'],
+      {body: "#{msg['body']}"}
+    )
     puts "RESULTS: #{results}\n\n"
   end
 rescue Aws::SQS::Errors::ServiceError => e
